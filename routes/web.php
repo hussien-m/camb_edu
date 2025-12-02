4f021ce213f229e813aeab806242cb13d2da6d1f
hussien-m <hussienalfeqawy@gmail.com>
2025-12-02 14:57:48 +0200

    Fix Code

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('test-push',function(){
-    return "Test 2";
+    return "Test 3";
 });
 // Frontend Routes
 Route::get('/', [HomeController::class, 'index'])->name('home');
