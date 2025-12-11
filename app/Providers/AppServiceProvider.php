5392b061bba7f03b9427c26747530bd70458496f
hussien-m <hussienalfeqawy@gmail.com>
2025-12-11 20:33:39 +0200

    Fix Images

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Register console commands
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                \App\Console\Commands\OptimizeImages::class,
+                \App\Console\Commands\ClearAppCache::class,
+            ]);
+        }
     }
 
     /**
